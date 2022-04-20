0731a7d14d078c301d523b14e3b63a30cd48d766
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-04-20 08:47:10 +0300

    update pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteDetailsComponent } from './quote-details/quote-details.component';
 import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
     QuoteDetailsComponent,
-    QuotesFormComponent
+    QuotesFormComponent,
+    DatePipePipe
   ],
   imports: [
     BrowserModule,
